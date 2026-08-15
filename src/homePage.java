beca2ccc782696b43134db69d1c0fffe321fba1f
GameProject <adirsaidov7@gmail.com>
2026-08-15 11:07:40 +0300

    gg

--- src/homePage.java
+++ src/homePage.java
@@ -4,7 +4,7 @@ import java.awt.*;
 public class homePage extends JPanel {
     public static final int BUTTON_WIDTH = 300;
     public static final int BUTTON_HEIGHT = 100;
-    public homePage(int width, int height) {
+    public homePage(int width, int height, JFrame frame) {
         this.setBounds(0,0,width,height);
         this.setBackground(Color.gray);
         this.setLayout(null);
@@ -17,19 +17,21 @@ public class homePage extends JPanel {
 
         JButton instractionsButton = new JButton("INSTRACTIONS");
         instractionsButton.setBounds(width/2 - BUTTON_WIDTH/2,height/3,BUTTON_WIDTH,BUTTON_HEIGHT);
-        instractionsButton.addActionListener(() -> {
-
-
+        instractionsButton.addActionListener( e -> {
+            pagesManager.switchBetweenPages(new instractionsScene(width, height),frame);
         });
         this.add(instractionsButton);
 
         JButton gameButton = new JButton("TO THE GAME");
         gameButton.setBounds(width/2 - BUTTON_WIDTH/2,height/3 * 2,BUTTON_WIDTH,BUTTON_HEIGHT);
+//        instractionsButton.addActionListener( e -> {
+//            switchBetweenPages(new gameScene(width,height), frame);
+//        });
         this.add(gameButton);
-
-
-
     }
+
+
+
 
 
 }
